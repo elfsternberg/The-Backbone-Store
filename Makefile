e448dd503e187bd1f0ddb38ec6249692c7239a57
Ken Sternberg <kenneth.m.sternberg@gmail.com>
2011-02-03 19:15:16 -0800

    Updated with smarter commentary and better code.

---
+++ Makefile
@@ -0,0 +1,58 @@
+.SUFFIXES: .nw .js .pdf .html .tex 
+
+NOTANGLE=		notangle
+NOWEAVE=		noweave
+ECHO=			/bin/echo
+
+all: index.html store.js 
+
+.nw.html:
+	$(NOWEAVE) -filter l2h -delay -x -index -autodefs c -html $*.nw > $*.html
+
+.nw.tex:
+	$(NOWEAVE) -x -delay $*.nw > $*.tex 			#$
+
+.tex.pdf:
+	xelatex $*.tex; \
+	while grep -s 'Rerun to get cross-references right' $*.log; \
+        do \
+		xelatex *$.tex; \
+	done
+
+.nw.js:
+	@ $(ECHO) $(NOTANGLE) -c -R$@ $<
+	@ - $(NOTANGLE) -c -R$@ $< > $*.nw-js-tmp
+	@ if [ -s "$*.nw-js-tmp" ]; then \
+		mv $*.nw-js-tmp $@; \
+	else \
+		echo "$@ not found in $<"; \
+	rm $*.nw-js-tmp; \
+	fi	
+
+store.js: backbonestore.nw
+	@ $(ECHO) $(NOTANGLE) -c -R$@ $<
+	@ - $(NOTANGLE) -c -R$@ $< > $*.nw-html-tmp
+	@ if [ -s "$*.nw-html-tmp" ]; then \
+		mv $*.nw-html-tmp $@; \
+	else \
+		echo "$@ not found in $<"; \
+	rm $*.nw-tmp; \
+	fi	
+
+index.html:
+	@ $(ECHO) $(NOTANGLE) -c -R$@ $<
+	@ - $(NOTANGLE) -c -R$@ $< > $*.nw-html-tmp
+	@ if [ -s "$*.nw-html-tmp" ]; then \
+		mv $*.nw-html-tmp $@; \
+	else \
+		echo "$@ not found in $<"; \
+	rm $*.nw-tmp; \
+	fi	
+
+
+clean:
+	- rm -f *.tex *.dvi *.aux *.toc *.log *.out *.html *.js
+
+realclean: clean
+	- rm -f *.pdf
+
--- store.js
+++ store.js
@@ -1,141 +1,152 @@
-var Product = Backbone.Model.extend({});
+(function() {
 
-var ProductCollection = Backbone.Collection.extend({
-    model: Product,
-    comparator: function(item) {
-        return item.get('title');
-    }
-});
-
-var CartItem = Backbone.Model.extend({
-    update: function(amount) {
-        this.set({'quantity': this.get('quantity') + amount});
-    }
-});
-
-
-var Cart = Backbone.Collection.extend({
-    model: CartItem,
-    getByPid: function(pid) {
-        return this.detect(function(obj) { return (obj.get('product').cid == pid); });
-    },
-});
-
-var CartView = Backbone.View.extend({
-    el: $('.cart-info'),
-
-    initialize: function() {
-        this.model.bind('change', _.bind(this.render, this));
-    },
-
-    render: function() {
-        var sum = this.model.reduce(function(m, n) { return m + n.get('quantity'); }, 0);
-        this.el
-            .find('.cart-items').text(sum).end()
-            .animate({paddingTop: '30px'})
-            .animate({paddingTop: '10px'});
-    }
-});
-
-var ProductView = Backbone.View.extend({
-    el: $('#main'),
-    itemTemplate: $("#itemTmpl").template(),
-
-    events: {
-        "keypress .uqf" : "updateOnEnter",
-        "click .uq"     : "update",
-    },
-
-    initialize: function(options) {
-        this.cart = options.cart;
-    },
-
-    update: function(e) {
-        e.preventDefault();
-        var cart_item = this.cart.getByPid(this.model.cid);
-        if (_.isUndefined(cart_item)) {
-            cart_item = new CartItem({product: this.model, quantity: 0});
-            this.cart.add(cart_item, {silent: true});
+    var Product = Backbone.Model.extend({
+        toJSON: function() {
+            return _.extend(_.clone(this.attributes), {cid: this.cid})
         }
-        cart_item.update(parseInt($('.uqf').val()));
-    },
+    });
 
-    updateOnEnter: function(e) {
-        if (e.keyCode == 13) {
-            return this.update(e);
+    var ProductCollection = Backbone.Collection.extend({
+        model: Product,
+
+        comparator: function(item) {
+            return item.get('title');
         }
-    },
+    });
 
-    render: function() {
-        var sg = this;
-        this.el.fadeOut('fast', function() {
-            sg.el.empty();
-            $.tmpl(sg.itemTemplate, sg.model).appendTo(sg.el);
-            sg.el.fadeIn('fast');
-        });
-        return this;
-    }
-});
+    var CartItem = Backbone.Model.extend({
+        update: function(amount) {
+            this.set({'quantity': this.get('quantity') + amount});
+        }
+    });
+
+    var Cart = Backbone.Collection.extend({
+        model: CartItem,
+        getByProductId: function(pid) {
+            return this.detect(function(obj) { return (obj.get('product').cid == pid); });
+        },
+    });
 
 
-var IndexView = Backbone.View.extend({
-    el: $('#main'),
-    indexTemplate: $("#indexTmpl").template(),
+    var CartView = Backbone.View.extend({
+        el: $('.cart-info'),
+        
+        initialize: function() {
+            this.collection.bind('change', _.bind(this.render, this));
+        },
+        
+        render: function() {
+            var sum = this.collection.reduce(function(m, n) { return m + n.get('quantity'); }, 0);
+            this.el
+                .find('.cart-items').text(sum).end()
+                .animate({paddingTop: '30px'})
+                .animate({paddingTop: '10px'});
+        }
+    });
 
-    render: function() {
-        var sg = this;
-        this.el.fadeOut('fast', function() {
-            sg.el.empty();
-            $.tmpl(sg.indexTemplate, sg.model.toArray()).appendTo(sg.el);
-            sg.el.fadeIn('fast');
-        });
-        return this;
-    }
 
-});
-
-var Workspace = Backbone.Controller.extend({
-    _index: null,
-    _products: null,
-    _cart :null,
-
-    routes: {
-        "": "index",
-        "item/:id": "item",
-    },
-
-    initialize: function() {
-        var ws = this;
-        if (this._index === null) {
-            $.ajax({
-                url: 'data/items.json',
-                dataType: 'json',
-                data: {},
-                success: function(data) {
-                    ws._cart = new Cart();
-                    new CartView({model: ws._cart});
-                    ws._products = new ProductCollection(data);
-                    ws._index = new IndexView({model: ws._products});
-                    Backbone.history.loadUrl();
-                }
+    var ProductListView = Backbone.View.extend({
+        el: $('#main'),
+        indexTemplate: $("#indexTmpl").template(),
+        
+        render: function() {
+            var self = this;
+            this.el.fadeOut('fast', function() {
+                self.el.html($.tmpl(self.indexTemplate, self.model.toJSON()));
+                self.el.fadeIn('fast');
             });
             return this;
         }
-        return this;
-    },
+    });
 
-    index: function() {
-        this._index.render();
-    },
 
-    item: function(id) {
-        if (_.isUndefined(this._products.getByCid(id)._view)) {
-            this._products.getByCid(id)._view = new ProductView({model: this._products.getByCid(id),
-                                                                 cart: this._cart});
+    var ProductView = Backbone.View.extend({
+        el: $('#main'),
+        itemTemplate: $("#itemTmpl").template(),
+
+        initialize: function(options) {
+            this.cart = options.cart;
+            return this;
+        },
+        
+        update: function(e) {
+            e.preventDefault();
+            var cart_item = this.cart.getByProductId(this.model.cid);
+            if (_.isUndefined(cart_item)) {
+                cart_item = new CartItem({product: this.model, quantity: 0});
+                this.cart.add(cart_item, {silent: true});
+            }
+            cart_item.update(parseInt($('.uqf').val()));
+        },
+        
+        updateOnEnter: function(e) {
+            if (e.keyCode == 13) {
+                return this.update(e);
+            }
+        },
+
+        events: {
+            "keypress .uqf" : "updateOnEnter",
+            "click .uq"     : "update",
+        },
+
+        render: function() {
+            var self = this;
+            this.el.fadeOut('fast', function() {
+                self.el.html($.tmpl(self.itemTemplate, self.model.toJSON()));
+                self.el.fadeIn('fast');
+            });
+            return this;
         }
-        this._products.getByCid(id)._view.render();
-    }
-});
+    });
 
-workspace = new Workspace();
-Backbone.history.start();
+
+    var BackboneStore = Backbone.Controller.extend({
+        _index: null,
+        _products: null,
+        _cart :null,
+        routes: {
+            "": "index",
+            "item/:id": "item",
+        },
+
+        initialize: function(data) {
+            this._cart = new Cart();
+            new CartView({collection: this._cart});
+            this._products = new ProductCollection(data);
+            this._index = new ProductListView({model: this._products});
+            return this;
+        },
+
+        index: function() {
+            this._index.render();
+        },
+
+        item: function(id) {
+            var product = this._products.getByCid(id);
+            if (_.isUndefined(product._view)) {
+                product._view = new ProductView({model: product,
+                                                 cart: this._cart});
+            }
+            product._view.render();
+        }
+    });
+
+
+    $(document).ready(function() {
+        var fetch_items = function() {
+            return $.ajax({
+                url: 'data/items.json',
+                data: {},
+                contentType: "application/json; charset=utf-8",
+                dataType: "json"
+            });
+        };
+
+        $.when(fetch_items()).then(function(data) {
+            new BackboneStore(data);
+            Backbone.history.start();
+        });
+    });
+
+}).call(this);
